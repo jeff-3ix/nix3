0b6c037519e654f7ad44ec1eb0b9719a63301c86
jeff-3ix <jeffryt@pm.me>
2025-12-20 23:10:36 -0500

    first backup. still futzing with ghostty

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    ghostty.url = "github:ghostty-org/ghostty";
+  };
+
+  outputs =
+    { nixpkgs, home-manager, ghostty, ... }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      nixosConfigurations = {
+        nixos = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              
+              # Pass ghostty and system to home.nix
+              home-manager.extraSpecialArgs = { inherit ghostty system; };
+              
+              home-manager.users.jeffu = ./home.nix;
+
+              # Optionally, use home-manager.extraSpecialArgs to pass
+              # arguments to home.nix
+            }
+          ];
+        };
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,209 @@
+{ config, pkgs, ghostty, system, ... }:
+
+{
+  # TODO please change the username & home directory to your own
+  home.username = "jeffu";
+  home.homeDirectory = "/home/jeffu";
+
+  # link the configuration file in current directory to the specified location in home directory
+  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
+
+  # link all files in `./scripts` to `~/.config/i3/scripts`
+  # home.file.".config/i3/scripts" = {
+  #   source = ./scripts;
+  #   recursive = true;   # link recursively
+  #   executable = true;  # make all files executable
+  # };
+
+  # encode the file content in nix configuration file directly
+  # home.file.".xxx".text = ''
+  #     xxx
+  # '';
+
+  # set cursor size and dpi for 4k monitor
+  # xresources.properties = {
+  #  "Xcursor.size" = 16;
+  #  "Xft.dpi" = 172;
+  # };
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    # here is some command line tools I use frequently
+    # feel free to add your own or remove some of them
+
+    neofetch
+    nnn # terminal file manager
+    ghostty.packages.${system}.default
+
+    # archives
+    zip
+    xz
+    unzip
+    p7zip
+
+    # fonts
+    nerd-fonts.fira-code
+    #nerd-fonts.font-logos
+    #nerd-fonts.powerline-extra-symbols
+    nerd-fonts.noto
+    nerd-fonts.ubuntu
+    nerd-fonts.jetbrains-mono
+
+    # utils
+    ripgrep # recursively searches directories for a regex pattern
+    jq # A lightweight and flexible command-line JSON processor
+    yq-go # yaml processor https://github.com/mikefarah/yq
+    eza # A modern replacement for ‘ls’
+    fzf # A command-line fuzzy finder
+    zoxide
+    trashy
+    tealdeer
+
+    # networking tools
+    mtr # A network diagnostic tool
+    iperf3
+    dnsutils  # `dig` + `nslookup`
+    ldns # replacement of `dig`, it provide the command `drill`
+    aria2 # A lightweight multi-protocol & multi-source command-line download utility
+    socat # replacement of openbsd-netcat
+    nmap # A utility for network discovery and security auditing
+    ipcalc  # it is a calculator for the IPv4/v6 addresses
+
+    # misc
+    cowsay
+    file
+    which
+    tree
+    gnused
+    gnutar
+    gawk
+    zstd
+    gnupg
+
+    # nix related
+    #
+    # it provides the command `nom` works just like `nix`
+    # with more details log output
+    nix-output-monitor
+
+    # productivity
+    hugo # static site generator
+    glow # markdown previewer in terminal
+
+    btop  # replacement of htop/nmon
+    iotop # io monitoring
+    iftop # network monitoring
+
+    # system call monitoring
+    strace # system call monitoring
+    ltrace # library call monitoring
+    lsof # list open files
+
+    # system tools
+    sysstat
+    lm_sensors # for `sensors` command
+    ethtool
+    pciutils # lspci
+    usbutils # lsusb
+  ];
+
+  # basic configuration of git, please change to your own
+  programs.git = {
+    enable = true;
+    settings = {
+      user.name = "jeff-3ix";
+      user.email = "jeffryt@pm.me";
+    };
+  };
+
+  fonts.fontconfig.enable = true;
+
+  programs.starship = {
+    enable = true;
+    # Configuration written to ~/.config/starship.toml
+    settings = {
+      # add_newline = false;
+
+      # character = {
+      #   success_symbol = "[➜](bold green)";
+      #   error_symbol = "[➜](bold red)";
+      # };
+
+      # package.disabled = true;
+    };
+  };
+  # alacritty - a cross-platform, GPU-accelerated terminal emulator
+  programs.alacritty = {
+    enable = true;
+    # custom settings
+    settings = {
+      env.TERM = "xterm-256color";
+      font = {
+        normal = { family = "JetBrains Mono Nerd Font"; style = "Regular"; };
+        bold = { family = "JetBrains Mono Nerd Font"; style = "Bold"; };
+        italic = { family = "JetBrains Mono Nerd Font"; style = "Italic"; };
+        bold_italic = { family = "JetBrains Mono Nerd Font"; style = "Bold Italic"; };
+        size = 12;
+        draw_bold_text_with_bright_colors = true;
+      };
+      scrolling.multiplier = 5;
+      selection.save_to_clipboard = true;
+    };
+  };
+  
+  programs.ghostty = {
+    enable = true;
+    package = ghostty.packages.${system}.default;
+    settings = {
+      background-blur-radius = 20;
+      theme = "dark:Catppuccin Mocha,light:Catppuccin Latte";
+      window-theme = "dark";
+      background-opacity = 0.8;
+      minimum-contrast = 1.1;
+      keybind = [
+        "global:ctrl+`=toggle_quick_terminal"
+        "global:ctrl+shift+n=new_window"
+      ];
+    };
+
+  };
+    
+  programs.kitty = {
+    enable = true;
+    font = {
+      name = "JetBrainsMono Nerd Font";
+      size = 12;
+    };
+  };   
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    # TODO add your custom bashrc here
+    bashrcExtra = ''
+      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+    '';
+
+    # set some aliases, feel free to add more or remove some
+    shellAliases = {
+      k = "kubectl";
+      urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
+      urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
+      ls = "eza";
+      ll = "eza -l";
+      la = "eza -a";
+      update = "sudo nixos-rebuild switch";
+      
+    };
+  };
+
+  # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "25.11";
+}
