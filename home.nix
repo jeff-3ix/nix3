c1ed35d7e289a920c5971d932264411fa29ee0c6
jeff-3ix <jeffryt@pm.me>
2025-12-21 15:48:37 -0500

    more keyboard shortcut fixes

--- home.nix
+++ home.nix
@@ -103,6 +103,8 @@
   dconf.settings = {
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings" = [
       "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0"
+      "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1"
+      "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" 
     ];
     # ghostty keyboard shortcut
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
