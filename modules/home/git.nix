24efccc1a031354a2dd701ec16fb6c1d2dceb862
jeff-3ix <jeffryt@pm.me>
2025-12-25 12:38:14 -0500

    try: allow git to automatically set upstreams with first push of new branch, in ~/nixos-config/modules/home/git.nix

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -3,6 +3,9 @@
 {
   programs.git = {
     enable = true;
+    extraConfig = {
+      push.autoSetupRemote = true;
+    };
     settings = {
       user.name = "jeff-3ix";
       user.email = "jeffryt@pm.me";
