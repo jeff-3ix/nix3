26e6c8b5ebbe658dde264c39076d842eab26a2aa
jeff-3ix <jeffryt@pm.me>
2025-12-25 12:15:25 -0500

    try: store github credentials in GNOME keyring to avoid ssh-askpass

--- modules/nixos/environment.nix
+++ modules/nixos/environment.nix
@@ -1,5 +1,16 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   environment.variables.EDITOR = "vim";
+
+  programs.git.enable = true;
+
+  # Credential storage via Secret Service (GNOME Keyring, etc.)
+  environment.systemPackages = [
+    pkgs.git-credential-libsecret
+  ];
+
+  programs.git.config = {
+    credential.helper = "libsecret";
+  };
 }
