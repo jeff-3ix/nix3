2c934b5d8bfd9bd7217233618dc465db87057e50
jeff-3ix <jeffryt@pm.me>
2025-12-25 11:01:18 -0500

    try: new aliases for rebuilds and nix cleanup

--- modules/home/bash.nix
+++ modules/home/bash.nix
@@ -8,7 +8,6 @@
       export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
     '';
 
-    # set some aliases, feel free to add more or remove some
     shellAliases = {
       # ls and eza aliases (requires eza)
       ls = "eza";
@@ -26,10 +25,36 @@
       te = "trash-empty";
       
       # nix update aliases
-      update = "sudo nixos-rebuild switch";
-      up = "sudo nixos-rebuild switch";
-      up-test = "sudo nixos-rebuild test";
-      
+      # normal rebuild
+      rebuild = "sudo nixos-rebuild switch --flake ~/nixos-config#nixos";
+      nr = "sudo nixos-rebuild switch --flake ~/nixos-config#nixos";
+
+      # test only
+      rebuild-test = "sudo nixos-rebuild test --flake ~/nixos-config#nixos";
+      nrt = "sudo nixos-rebuild test --flake ~/nixos-config#nixos";
+
+      # next-boot only
+      rebuild-boot = "sudo nixos-rebuild boot --flake ~/nixos-config#nixos";
+      nrb = "sudo nixos-rebuild boot --flake ~/nixos-config#nixos";  
+
+      # debugging mode
+      nr-trace = "sudo nixos-rebuild switch --flake ~/nixos-config#nixos --show-trace";
+
+      # clean system generations, then clean user generations, then rebuild
+      nr-clean = ''
+        sudo nix-collect-garbage -d && 
+        nix-collect-garbage -d &&
+        sudo nixos-rebuild switch --flake ~/nixos-config#nixos
+      ''; 
+
+      # deep clean
+      nr-nuke = ''
+        sudo nix-collect-garbage -d &&
+        nix-collect-garbage -d &&
+        sudo nix-store --optimise &&
+        sudo nixos-rebuild switch --flake ~/nixos-config#nixos
+      ''; 
+    
       # git aliases
       gs = "git status";
       gc = "git commit -am";
