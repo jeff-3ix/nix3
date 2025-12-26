fef8a42c3efb26959d4ee54097bd399c3ae73149
jeff-3ix <jeffryt@pm.me>
2025-12-25 23:10:15 -0500

    refactor: added vim.nix and home/default.nix

--- home/jeffu/default.nix
+++ home/jeffu/default.nix
@@ -2,21 +2,7 @@
 
 {
   imports = [
-    ../../modules/home/base.nix
-
-    ../../modules/home/packages-cli.nix
-    ../../modules/home/packages-gui.nix
-    ../../modules/home/packages-dev.nix
-
-    ../../modules/home/ghostty.nix
-    ../../modules/home/zoxide.nix
-    ../../modules/home/bash.nix
-    ../../modules/home/git.nix
-    ../../modules/home/starship.nix
-
-    ../../modules/home/services/warm-citrix-distrobox.nix
-
-    # This module contains your existing commented-out dconf stanza (kept verbatim).
-    ../../modules/home/dconf-commented.nix
+    ../../modules/home
   ];
 }
+
