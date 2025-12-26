ef66ff65a5d80859677d66f0488790b434ed9c24
jeff-3ix <jeffryt@pm.me>
2025-12-25 23:29:14 -0500

    fix(home/vim): use extraConfig for all vim options

--- modules/home/vim.nix
+++ modules/home/vim.nix
@@ -1,62 +1,56 @@
-{ config, pkgs, ... }:
+{ ... }:
 
 {
   programs.vim = {
     enable = true;
 
-    # Keep only options that Home Manager's vim module exposes reliably.
-    settings = {
-      number = true;
-
-      hlsearch = true;
-      incsearch = true;
-      ignorecase = true;
-      smartcase = true;
-
-      expandtab = true;
-      tabstop = 2;
-      softtabstop = 2;
-      shiftwidth = 2;
-
-      textwidth = 79;
-
-      mouse = "a";
-      cursorline = true;
-      scrolloff = 6;
-      signcolumn = "yes";
-
-      clipboard = "unnamed";
-    };
-
-    # Everything else goes here as plain vimrc.
     extraConfig = ''
       set fileformat=unix
       set encoding=UTF-8
       syntax on
 
       set nocompatible
+      set number
       set linebreak
       set showbreak=+++
+      set textwidth=79
       set showmatch
       set visualbell
 
-      " indentation behavior
+      set hlsearch
+      set smartcase
+      set ignorecase
+      set incsearch
+
+      " Defaults: 2-space indent
+      set tabstop=2
+      set softtabstop=2
+      set shiftwidth=2
+
       set autoindent
       set smartindent
       set smarttab
-
+      set expandtab
       set nowrap
+
       set ruler
       set undolevels=1000
       set backspace=indent,eol,start
+
+      inoremap jj <esc>
+
+      set mouse=a
+      set cursorline
+      set scrolloff=6
+      set signcolumn=yes
+
+      set clipboard=unnamed
 
       " Python: 4-space indent
       augroup ft_python_indent
         autocmd!
         autocmd FileType python setlocal tabstop=4 softtabstop=4 shiftwidth=4
       augroup END
-
-      inoremap jj <esc>
     '';
   };
 }
