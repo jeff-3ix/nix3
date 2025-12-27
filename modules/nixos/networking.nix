b329014995eba70928f10624071a3671c70dc287
jeff-3ix <jeffryt@pm.me>
2025-12-27 13:32:23 -0500

    try(networking): changes to clear up networking setup

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, lib, ... }:
 
 {
   # Networking
@@ -8,11 +8,15 @@
       enable = true;   # Enable networking
       waitOnline.enable = false;  # system reaches multi-user targets sooner on boot
     };
-    firewall = {
+    firewall = lib.mkIf config.services.tailscale.enable {
       enable = true;  # enable the firewall
       trustedInterfaces = [ "tailscale0" ];  # always allow traffic from your Tailscale network
       allowedUDPPorts = [ config.services.tailscale.port ]; # allow the Tailscale UDP port through the firewall
       # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
     };
+  };
+
+  services = {
+    modemmanager.enable = false; # don't need cellular modem, speed up boot
   };
 }
