e940fea0bbf4b71a8ac36985472c2f736d28195d
jeff-3ix <jeffryt@pm.me>
2025-12-27 14:54:37 -0500

    try(boot): added swap in modules/nixos/boot.nix

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -8,4 +8,8 @@
     kernelPackages = pkgs.linuxPackages_latest; # Use latest kernel.
     initrd.luks.devices."luks-root".device = "/dev/disk/by-uuid/06eeab67-7475-484b-b82b-139750d1d5e0";
   };
+
+  swapDevices = [
+    { device = "/swapfile"; size = 8192; }
+  ];
 }
