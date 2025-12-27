b87950864e43d5093fcaa77a6dac52ceda2dd43a
jeff-3ix <jeffryt@pm.me>
2025-12-27 11:46:25 -0500

    try(bluetooth): turn off systemd bluetooth and blueman to speed boot time, in modules/nixos/services-desktop.nix

--- modules/nixos/services-desktop.nix
+++ modules/nixos/services-desktop.nix
@@ -23,52 +23,61 @@
 
     openssh.enable = true;      # Enable the OpenSSH Daemon
     # xserver.libinput.enable = true; # Enable touchpad support (enabled default in most desktopManager).
+
+    printing = {
+      enable = true;    # Enable CUPS to print documents
+      drivers = [
+        pkgs.hplip
+        pkgs.hplipWithPlugin
+        pkgs.gutenprintpkgs.gutenprintBin
+      ];
+      listenAddresses = [ "*:631" ];
+      allowFrom = [ "all" ];
+      browsing = true;
+      defaultShared = true;
+      openFirewall = true;
+    };
+
+    avahi = {
+      enable = true;  # Enable Avahi autodiscovery of printers
+      nssmdns4 = true;
+      openFirewall = true;
+      publish.enable = true;
+      publish.userServices = true;
+    };
+
+    tailscale = {
+      enable = true;
+      useRoutingFeatures = "client";  # allow use of subnet routers / exit nodes
+    };
+
+    pulseaudio.enable = false;    # Enable sound with pipewire
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pusle.enable = true;
+    };
+
+    blueman.enable = false;   # Change to true to allow tray & agent on GNOME
   };
 
   programs.firefox.enable = true;
-
-  services.printing = {
-    enable = true;     # Enable CUPS to print documents
-    drivers = [
-      pkgs.hplip
-      pkgs.hplipWithPlugin
-      pkgs.gutenprint
-      pkgs.gutenprintBin
-    ];   # Add HP printer drivers
-    listenAddresses = [ "*:631" ];
-    allowFrom = [ "all" ];
-    browsing = true;
-    defaultShared = true;
-    openFirewall = true;
-  };
-
-  services.avahi = {
-    enable = true;    # Enable Avahi autodiscovery of printers
-    nssmdns4 = true;
-    openFirewall = true;
-    publish = {
-      enable = true;
-      userServices = true;
-    };
-  };
-
-  services.tailscale = {
-    enable = true;        # Enable Tailscale
-    useRoutingFeatures = "client";    # allow use of subn...et routers/exit nodes. Options are "server", "client", or "both"
-  };
 
   # nextdns = {
   #   enable = true;
   #   arguments = [ "-config" "8c527c" "-cache-siz" "10MB" ];
   # };
 
-  services.pulseaudio.enable = false;      # Enable sound with pipewire
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
   security.rtkit.enable = true;     # Needed for enabling pipewire
+
+  # Prevent Bluetooth from auto-starting at boot
+  systemd.services.bluetooth.wantedBy = [];
+
+  # Hardware
+  # Enable bluetooth (but prevent systemd and blueman from autostart - see above)
+  # To start bluetooth: 'sudo systemctl start bluetooth'
+  # To stop bluetooth: 'sudo systemctl stop bluetooth'
+  hardware.bluetooth.enable = true;   
 }
