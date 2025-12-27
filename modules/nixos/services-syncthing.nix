d8da49cdecf326703461f6cf755b87565c5bba02
jeff-3ix <jeffryt@pm.me>
2025-12-27 13:59:04 -0500

    try(syncthing): delay syncthing start until after boot so it doesn't hold up boot sequence

--- modules/nixos/services-syncthing.nix
+++ modules/nixos/services-syncthing.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   services.syncthing = {
@@ -19,5 +19,26 @@
       };
     };
   };
-}
 
+  # Don’t let Syncthing be pulled into boot targets directly
+  systemd.services.syncthing.wantedBy = lib.mkForce [ ];
+
+  # Start it shortly after boot instead (won’t block graphical.target)
+  systemd.services.syncthing-delayed-start = {
+    description = "Start Syncthing after boot (delayed)";
+    serviceConfig.Type = "oneshot";
+    script = ''
+      ${config.systemd.package}/bin/systemctl start syncthing.service
+    '';
+  };
+
+  systemd.timers.syncthing-delayed-start = {
+    description = "Delay Syncthing start to avoid blocking boot";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnBootSec = "20s";          # tweak: 10s / 20s / 30s
+      AccuracySec = "10s";
+      Unit = "syncthing-delayed-start.service";
+    };
+  };
+}
