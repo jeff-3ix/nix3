783608f569ee2e509f24e1630a41f496d5778c7d
jeff-3ix <jeffryt@pm.me>
2025-12-27 15:04:28 -0500

    try(podman): removed podman example container holding up boot in modules/nixos/virtualization.nix

--- modules/nixos/virtualisation.nix
+++ modules/nixos/virtualisation.nix
@@ -9,12 +9,6 @@
       defaultNetwork.settings.dns_enabled = true;
     };
     oci-containers.backend = "podman";
-    oci-containers.containers = {
-      container-name = {
-        image = "container-image";
-        autoStart = true;
-        ports = [ "127.0.0.1:1234:1234" ];
-      };
-    };
+    
   };
 }
