3c1cdd159ebc4f0b43c7316053c42b24a583d6c1
jeff-3ix <jeffryt@pm.me>
2025-12-28 15:06:34 -0500

    try(boot): added boot.kernelParams = [ nosgx ]; to boot.nix

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -6,6 +6,7 @@
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
     kernelPackages = pkgs.linuxPackages_latest; # Use latest kernel.
+    kernelParams = [ "nosgx" ];
     initrd.luks.devices."luks-root".device = "/dev/disk/by-uuid/06eeab67-7475-484b-b82b-139750d1d5e0";
   };
 
