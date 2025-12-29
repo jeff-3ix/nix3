5726dc223fd7ca97b4e91819dbb5db03b040e32c
jeff-3ix <jeffryt@pm.me>
2025-12-28 20:21:06 -0500

    try(nix): tuning download buffer size, parallel downloads, caching

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -7,11 +7,25 @@
   };
 
   nix = {
-    settings.experimental-features = [ "nix-command" "flakes"];
+    settings = {
+      experimental-features = [ "nix-command" "flakes"];
+
+      download-buffer-size = 268435456; # 256 MiB (16 GB RAM system)
+
+      narinfo-cache-negative-ttl = 300; # 5 minutes
+
+      max-jobs = "auto";
+      cores = 0;
+
+      keep-outputs = true;
+      keep-derivations = true;
+
+      connect-timeout = 5;
+    };
     gc = {
       automatic = true;
       dates = "weekly";
-      options = "--delete-older-than 30d";
+      options = "--delete-older-than 14d";
     };
   };
 }
