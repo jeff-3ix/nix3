b2d8e6a1bfbe868762a4b6ca280d0e4508c67db0
jeff-3ix <jeffryt@pm.me>
2025-12-30 22:41:26 -0500

    chore: removed ssh and pkcs11 from keyring.nix

--- modules/home/keyring.nix
+++ modules/home/keyring.nix
@@ -1,10 +1,6 @@
 {
-  home.sessionVariables = {
-    SSH_AUTH_SOCK = "$XDG_RUNTIME_DIR/keyring/ssh";
-  };
-
   services.gnome-keyring = {
     enable = true;
-    components = [ "pkcs11" "secrets" "ssh" ];
+    components = [ "secrets" ];
   };
 }
