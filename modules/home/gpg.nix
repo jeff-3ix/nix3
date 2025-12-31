a9a74ae7e1d1b7db11957d73c575d7f3da042798
jeff-3ix <jeffryt@pm.me>
2025-12-31 13:22:13 -0500

    fix(gpg/ssh): new name for pinentry.package

--- modules/home/gpg.nix
+++ modules/home/gpg.nix
@@ -13,7 +13,7 @@
 
     defaultCacheTtl = 600;          # example: 10 minutes
     maxCacheTtl = 7200;             # example: 2 hours
-    pinentryPackage = pkgs.pinentry-gnome3;
+    pinentry.package = pkgs.pinentry-gnome3;
   };
 }
 
