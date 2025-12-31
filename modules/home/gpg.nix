41524c40333d331741f8b472f62caf7a008035fe
jeff-3ix <jeffryt@pm.me>
2025-12-31 12:49:18 -0500

    try(gpg/ssh): working on making gpg manage ssh

--- modules/home/gpg.nix
+++ modules/home/gpg.nix
@@ -9,8 +9,6 @@
     enable = true;
     
     enableSshSupport = true;       # or false if you want keyring.nix to handle SSH
-   # sshKeys = [ "id_ed25519" ];    # optional: automatically add your SSH key
-    # on login
 
     defaultCacheTtl = 600;          # example: 10 minutes
     maxCacheTtl = 7200;             # example: 2 hours
