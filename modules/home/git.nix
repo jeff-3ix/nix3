29c6516a7c7e36060b14c1da7a45e76de97b11b4
jeff-3ix <jeffryt@pm.me>
2025-12-31 14:18:01 -0500

    try(gpg): adding gpg signing to git

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -3,12 +3,21 @@
 {
   programs.git = {
     enable = true;
+
+    userName = "jeff-3ix";
+    userEmail = "jeffryt@pm.me";
+
+    signing = {
+      key = "A84DA197C1543082F3E83D1DF1E1EEDAA53EE303"
+      signByDefault = true;
+    };
+
     settings = {
       push.autoSetupRemote = true;
-    };
-    settings = {
-      user.name = "jeff-3ix";
-      user.email = "jeffryt@pm.me";
+      pull.rebase = true;
+
+      gpg.format = "openpgp";
+      pgp.program = "gpg";
     };
   };
 }
