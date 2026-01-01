95afd959297ab383faa8b2c6dc01e38ab7f01d64
jeff-3ix <jeffryt@pm.me>
2025-12-31 23:10:35 -0500

    feat(gpg): changed gpg login to 24 hr since last use, 7 days max

--- modules/home/gpg.nix
+++ modules/home/gpg.nix
@@ -11,8 +11,8 @@
     enableSshSupport = true;       # or false if you want keyring.nix to handle SSH
     sshKeys = [ "FCF10CA3DA4D2C6753394DE4CC5E77ED83E0C031" ];
 
-    defaultCacheTtl = 600;          # example: 10 minutes
-    maxCacheTtl = 7200;             # example: 2 hours
+    defaultCacheTtl = 86400;          # 24 hours 
+    maxCacheTtl = 604800;             # 7 days
     pinentry.package = pkgs.pinentry-gnome3;
   };
 }
